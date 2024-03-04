f386200612e5ff9765dda9d9b953217244207a6b
xxx <yyy>
2024-03-04 23:04:55 +0800

    BubbleSort

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    Sort sort = new Sort();    
+    
+    int[] array = {38,65,3,17,2};
+    sort.main(array);
+    
+    
+    }
+    
+}
